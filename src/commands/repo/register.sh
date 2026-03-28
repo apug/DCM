c1b71f79a4c0a49336329d8f2992d8ed6c425d95
Andrea <andrea.puggioni@proton.me>
2026-03-28 09:03:02 +0100

    fix: remove custom directory name from repo register and add to enforce URL-derived naming

--- src/commands/repo/register.sh
+++ src/commands/repo/register.sh
@@ -4,13 +4,8 @@ load_env
 url="${args[url]}"
 branch="${args[--branch]:-}"
 
-# Determine directory name
-if [ -n "${args[directory]}" ]; then
-  name="${args[directory]}"
-else
-  repo_name=$(basename "$url" .git)
-  name=$(to_pascal_case "$repo_name")
-fi
+repo_name=$(basename "$url" .git)
+name=$(to_pascal_case "$repo_name")
 
 repos_register "$name" "$url" "$branch"
 
